3f63e44af264600a8572bdcaa3b2749c27641790
lin pascal <dunlintan@hk01.com>
2020-09-24 15:06:55 +0800

    refactor: layout and add game panel

---
+++ internal/cui/gamepanel.go
@@ -0,0 +1,70 @@
+package cui
+
+import (
+	"strings"
+
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+
+	"github.com/pascallin/go-wolvesgame/internal/game"
+)
+
+type GamePanel struct {
+	table *tview.Table
+	offset int
+	players string
+}
+
+var (
+	NewTitleCell = func(value string) *tview.TableCell {
+		return tview.NewTableCell(value).SetTextColor(tcell.ColorOrange).SetAlign(tview.AlignCenter)
+	}
+	NewValueCell = func(value string) *tview.TableCell {
+		return tview.NewTableCell(value).SetTextColor(tcell.ColorOrange).SetAlign(tview.AlignCenter)
+	}
+)
+
+func NewGamePanel() *GamePanel {
+
+	table := tview.NewTable().SetBorders(true)
+
+	table.SetCell(0, 0, NewTitleCell("Game:"))
+	table.SetCell(0, 1, NewValueCell("Status"))
+	table.SetCell(1, 1, NewValueCell("Non"))
+
+	table.SetCell(3, 0, NewTitleCell("Players:"))
+	var rowOffset = 4
+
+	header := strings.Split("ID Name Status", " ")
+	for i := range header {
+		table.SetCell(rowOffset, i,
+			tview.NewTableCell(header[i]).
+				SetTextColor(tcell.ColorYellow).
+				SetAlign(tview.AlignCenter))
+	}
+
+	return &GamePanel{
+		table: table,
+		offset: rowOffset + 1,
+	}
+}
+
+func (gp *GamePanel) UpdateGameStatus(status game.Status) {
+	gp.table.SetCell(1, 1, NewValueCell(status.String()))
+}
+
+/**
+playersData: column data sep with " " and row data sep with ","
+example:
+	NewGamePanel("1 Player1 Dead,2 Player2 Alive,3 Test Talking")
+*/
+func (gp *GamePanel) AddPlayerToGamePanel(playerData string) {
+	row := strings.Split(playerData, " ")
+	for c, v := range row {
+		gp.table.SetCell(1 + gp.offset, c,
+			tview.NewTableCell(v).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignCenter))
+	}
+	gp.offset++
+}
--- internal/cui/layout.go
+++ internal/cui/layout.go
@@ -5,17 +5,17 @@ import (
 )
 
 func NewFlexLayout(commandInput *tview.InputField, commandResult *tview.TextView, messageList *tview.TextView) (*tview.Flex) {
-	NewSection := func (item tview.Primitive) *tview.Grid {
-		return tview.NewGrid().
-			SetRows(3, 0, 3).
-			SetColumns(30, 0, 30).
-			AddItem(item, 0, 0, 3, 3, 1, 1, false)
-	}
+
+	gamePanel := NewGamePanel()
+
+	leftSide := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(commandInput, 0, 1, false).
+		AddItem(gamePanel.table, 0, 1, false)
 
 	flex := tview.NewFlex().
-		AddItem(NewSection(commandInput), 0, 1, false).
-		AddItem(NewSection(commandResult), 0, 1, false).
-		AddItem(NewSection(messageList), 0, 1, false)
+		AddItem(leftSide, 0, 1, false).
+		AddItem(commandResult, 0, 1, false).
+		AddItem(messageList, 0, 1, false)
 
 	return flex
 }
